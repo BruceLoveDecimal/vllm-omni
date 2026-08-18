a5994c5ca20a3875b57edb4ce105239a8b8363d8
liuqihao <liuqihao970610@gmail.com>
2026-08-19 00:58:45 +0800

    [Misc] Export Mage-VL config and processor
    Signed-off-by: liuqihao <liuqihao970610@gmail.com>

--- vllm_omni/transformers_utils/processors/__init__.py
+++ vllm_omni/transformers_utils/processors/__init__.py
@@ -1,12 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2025 The vLLM-Omni team.
 
+from vllm_omni.transformers_utils.processors.mage_vl import MageVLProcessor
 from vllm_omni.transformers_utils.processors.ming import (
     MingFlashOmniProcessor,
     MingWhisperFeatureExtractor,
 )
 
 __all__ = [
+    "MageVLProcessor",
     "MingFlashOmniProcessor",
     "MingWhisperFeatureExtractor",
 ]
